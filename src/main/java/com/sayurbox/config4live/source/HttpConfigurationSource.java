66427cf1a1944b7dade56cd58096af2f07ad34e4
mikail <81550295+mikailrrrrr@users.noreply.github.com>
2021-09-16 13:03:52 +0700

    add builder for HttpConfigurationSource

--- src/main/java/com/sayurbox/config4live/source/HttpConfigurationSource.java
+++ src/main/java/com/sayurbox/config4live/source/HttpConfigurationSource.java
@@ -27,4 +27,55 @@ public class HttpConfigurationSource implements ConfigurationSource {
         HttpServiceCommand cmd = new HttpServiceCommand(this.httpClient, url, key, hystrixParams);
         return cmd.execute();
     }
+
+    public static class Builder {
+        private String httpUrl;
+        private Integer hystrixExecutionTimeout = 1000;
+        private Integer hystrixCircuitBreakerSleepWindow = 1000;
+        private Integer hystrixCircuitBreakerRequestVolumeThreshold = 10;
+        private Integer hystrixRollingStatisticalWindow = 10000;
+        private Integer hystrixHealthSnapshotInterval = 500;
+
+        public Builder() {}
+
+        public Builder withUrl (String url) {
+            httpUrl = url;
+            return this;
+        }
+
+        public Builder withHystrixExecutionTimeout(int hystrixExecutionTimeout) {
+            this.hystrixExecutionTimeout = hystrixExecutionTimeout;
+            return this;
+        }
+
+        public Builder withHystrixCircuitBreakerSleepWindow(int hystrixCircuitBreakerSleepWindow) {
+            this.hystrixCircuitBreakerSleepWindow = hystrixCircuitBreakerSleepWindow;
+            return this;
+        }
+
+        public Builder withHystrixCircuitBreakerRequestVolumeThreshold(
+                int hystrixCircuitBreakerRequestVolumeThreshold) {
+            this.hystrixCircuitBreakerRequestVolumeThreshold = hystrixCircuitBreakerRequestVolumeThreshold;
+            return this;
+        }
+
+        public Builder withHystrixRollingStatisticalWindow(int hystrixRollingStatisticalWindow) {
+            this.hystrixRollingStatisticalWindow = hystrixRollingStatisticalWindow;
+            return this;
+        }
+
+        public Builder withHystrixHealthSnapshotInterval(int hystrixHealthSnapshotInterval) {
+            this.hystrixHealthSnapshotInterval = hystrixHealthSnapshotInterval;
+            return this;
+        }
+
+        public HttpConfigurationSource build() {
+            HystrixParams hystrixParams =
+                    new HystrixParams(hystrixExecutionTimeout, hystrixCircuitBreakerSleepWindow,
+                            hystrixCircuitBreakerRequestVolumeThreshold, hystrixRollingStatisticalWindow,
+                            hystrixHealthSnapshotInterval);
+            return new HttpConfigurationSource(httpUrl, hystrixParams);
+
+        }
+    }
 }
